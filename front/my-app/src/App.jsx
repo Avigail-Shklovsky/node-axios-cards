d6a035baf587b9dd28b8997e2b8bd0b78b37108a
Avigail Shklovsky <9013825@gmail.com>
2024-10-30 15:00:36 +0200

    styled

--- front/my-app/src/App.jsx
+++ front/my-app/src/App.jsx
@@ -6,8 +6,10 @@ import './App.css'
 function App() {
   const [cards, setCards] = useState([]);
   const [text, setText] = useState("");
-  const [color, setColor] = useState("#000000");
+  const [color, setColor] = useState("#403d3d");
   const [currentId, setCurrentId] = useState(null);
+  const [isEditing, setIsEditing] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   // Fetch all cards
   useEffect(() => {
@@ -37,7 +39,8 @@ function App() {
         setCards([...cards, response.data]);
       }
       setText("");
-      setColor("#000000");
+      setColor("#403d3d");
+      setIsModalOpen(false);
     } catch (error) {
       console.error("Error saving card:", error);
     }
@@ -53,50 +56,117 @@ function App() {
     }
   };
 
-  // Set card for editing
-  const editCard = (card) => {
+  const handleTextClick = (card) => {
     setCurrentId(card.id);
+    setColor(card.color)
     setText(card.text);
-    setColor(card.color);
+    setIsEditing(true);
   };
+
+  const handleTextBlur = async () => {
+    if (currentId) {
+      await saveCard();
+    }
+    setIsEditing(false);
+  };
+
+  const resetState = () => {
+    setCurrentId(null);
+    setText("");
+    setColor("#403d3d");
+    setIsEditing(false);
+  };
+
 
   return (
     <div className="app">
       <h1 className="title">Card Manager</h1>
 
-      <div className="input" >
-        <input
-          type="text"
-          placeholder="Text"
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-        />
-        <input
-          type="color"
-          value={color}
-          onChange={(e) => setColor(e.target.value)}
-        />
-        <button onClick={saveCard}>{currentId ? "Update Card" : "Add Card"}</button>
-      </div>
+
+      {isModalOpen && (
+        <div className="modal">
+
+          <div className="modalContent">
+            <button className="closeModal" onClick={() => setIsModalOpen(false)}>
+              <Icon icon="mingcute:close-fill" className="closeModalIcon" />
+            </button>
+
+            <h1 style={{ color: 'white' }}>Add Card:</h1>
+            <input className="textAdd"
+              type="text"
+              placeholder="Text"
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+            />
+            <div className="colorDiv">
+              <p style={{ color: 'white', fontSize: 'large' }}>Choose background color:</p>
+
+              <input className="colorInput"
+                type="color"
+                value={color}
+                onChange={(e) => setColor(e.target.value)}
+              />
+            </div>
+            <button className="saveCard" onClick={saveCard}>{currentId ? "Update Card" : "Add Card"}</button>
+
+          </div>
+        </div>
+      )}
+
+
 
       <div className="cardsContainer" >
         {cards.map((card) => (
-          <div className="card"
-            key={card.id}
-            style={{ backgroundColor: card.color }}
-          >
-            <p className="cardText">{card.text}</p>
+          <div className="card" key={card.id} style={{ backgroundColor: card.color }}>
+
+            {isEditing && currentId === card.id ? (
+              <input className="textUpdate"
+                type="text"
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+                onBlur={handleTextBlur}
+                autoFocus
+              />
+            ) : (
+              <p className="cardText" onClick={() => handleTextClick(card)}>
+                {card.text}
+              </p>
+            )}
+
             <div className="buttonsContainer">
-              <button className="cardButtons" onClick={() => editCard(card)}>
-                <Icon className='icon'icon="tabler:edit" />
-              </button>
-              <button className="cardButtons" onClick={() => deleteCard(card.id)}>
+
+              <input
+                className="colorUpdate"
+                type="color"
+                value={card.color}
+                onChange={(e) => {
+                  const newColor = e.target.value;
+                  setColor(newColor);
+                  setCards(cards.map((c) => (c.id === card.id ? { ...c, color: newColor } : c)));
+                }}
+                onBlur={async (e) => {
+                  const newColor = e.target.value; // Get the color value on blur
+                  await axios.put(`http://localhost:5000/cards/${card.id}`, { text: card.text, color: newColor });
+                }}
+                style={{ cursor: 'pointer' }}
+              />
+
+              <button className="deleteButton" onClick={() => deleteCard(card.id)}>
                 <Icon className='icon' icon="tabler:trash" />
               </button>
+
             </div>
+
           </div>
         ))}
+        <button className="addCard" onClick={() => {
+          resetState();
+          setIsModalOpen(true);
+        }}>
+          <Icon className='plus' icon="rivet-icons:plus" />
+        </button>
       </div>
+
     </div>
   );
 }
